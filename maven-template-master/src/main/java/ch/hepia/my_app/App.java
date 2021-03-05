a77a268ee0abdf4d4a2c1407b81c52b5f1d1f3f6
gustavo <gustavo.pertuzati@etu.hesge.ch>
2021-03-05 14:14:31 +0100

    Initial commit

---
+++ maven-template-master/src/main/java/ch/hepia/my_app/App.java
@@ -0,0 +1,11 @@
+package ch.hepia.my_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
