910b84a7081d4aa2d68ab40ac5a9d299c1ba0d7d
gustavo <gustavo.pertuzati@etu.hesge.ch>
2021-03-12 13:59:24 +0100

    test javafx

--- maven-template-master/src/main/java/ch/hepia/my_app/App.java
+++ maven-template-master/src/main/java/ch/hepia/my_app/App.java
@@ -1,11 +1,35 @@
 package ch.hepia.my_app;
 import java.util.Map;
 
-public class App {
-    public static void main( String[] args ) {
-        APICountryManager test = new APICountryManager("https://api.covid19api.com");
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-        Map<String, Country> lol = test.getCountryMap("summary");
-        lol.forEach( (k,v) -> System.out.println(v));
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
