42df5becd86ab388688b4c98367e09680e6c4650
gustavo <gustavo.pertuzati@etu.hesge.ch>
2021-03-19 13:32:48 +0100

    Latest modifs of worldmap

--- maven-template-master/src/main/java/ch/hepia/my_app/interface/Worldmap.java
+++ maven-template-master/src/main/java/ch/hepia/my_app/interface/Worldmap.java
@@ -15,6 +15,8 @@ import javafx.geometry.Rectangle2D;
 import javafx.geometry.Insets;
 import javafx.scene.text.Text;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -23,10 +25,10 @@ class NewStage {
 
     NewStage(Country c, Stage primaryStage)
     {
-        Stage subStage = new Stage();
-        subStage.setTitle(c.CountryName());
+        Stage detailStage = new Stage();
+        detailStage.setTitle(c.CountryName());
 
-        Text text = new Text("\n\n" + c.toString());
+        Text countryDetail = new Text("\n\n" + c.toString());
 
         Button btn1 = new Button();
         btn1.setText("More options");
@@ -47,42 +49,68 @@ class NewStage {
             }
         });
 
-        VBox root = new VBox();
+        VBox detailRoot = new VBox();
         
-        root.getChildren().add(text);
+        detailRoot.getChildren().add(countryDetail);
         
         btn1.setLayoutX(900);
         btn1.setLayoutY(530);
-        root.getChildren().add(btn1);
+        detailRoot.getChildren().add(btn1);
         
         btn2.setLayoutX(900);
         btn2.setLayoutY(630);
-        root.getChildren().add(btn2);
+        detailRoot.getChildren().add(btn2);
 
-        root.setAlignment(Pos.CENTER);
-        VBox.setMargin(text, new Insets(10, 10, 10, 10));
+        detailRoot.setAlignment(Pos.CENTER);
+        VBox.setMargin(countryDetail, new Insets(10, 10, 10, 10));
         VBox.setMargin(btn2, new Insets(10, 10, 10, 10));
         VBox.setMargin(btn1, new Insets(10, 10, 10, 10));
 
-        Scene scene = new Scene(root, 1024, 800);
+        Scene detailScene = new Scene(detailRoot, 800, 400, Color.BLACK);
         
-        subStage.setScene(scene);
-        subStage.initModality(Modality.APPLICATION_MODAL);
-        subStage.initOwner(primaryStage);
-        subStage.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
-            if (! isNowFocused) {
-                subStage.hide();
-            }
-        });
-
+        detailStage.setScene(detailScene);
+        detailStage.initModality(Modality.APPLICATION_MODAL);
+        detailStage.initOwner(primaryStage);
+        
         Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
  
         //set Stage boundaries to the lower right corner of the visible bounds of the main screen
-        subStage.setX(primaryScreenBounds.getMinX() + primaryScreenBounds.getWidth() - 400);
-        subStage.setY(primaryScreenBounds.getMaxY() - primaryScreenBounds.getHeight() - 300);
-        subStage.setWidth(400);
-        subStage.setHeight(900);
+        detailStage.setX(primaryScreenBounds.getMinX() + primaryScreenBounds.getWidth() - 400);
+        detailStage.setY(primaryScreenBounds.getMaxY() - primaryScreenBounds.getHeight() - 300);
+        detailStage.setWidth(400);
+        detailStage.setHeight(1000);
+        /*
+        Stage newsStage = new Stage();
+        newsStage.setTitle("Latest news from " + c.CountryName());
+        
+        Text news = new Text("Bad news...");
+        
+        VBox newsRoot = new VBox();
+        
+        newsRoot.getChildren().add(news);
+        
+        newsRoot.setAlignment(Pos.CENTER_LEFT);
+        VBox.setMargin(news, new Insets(10, 10, 10, 10));
+        
+        Scene newsScene = new Scene(newsRoot, 1024, 800);
+        
+        newsStage.setScene(newsScene);
+        newsStage.initModality(Modality.APPLICATION_MODAL);
+        newsStage.initOwner(primaryStage);
+        
+        newsStage.setX(primaryScreenBounds.getMaxX() + primaryScreenBounds.getWidth() - 400);
+        newsStage.setY(primaryScreenBounds.getMinY() - primaryScreenBounds.getHeight() - 300);
+        newsStage.setWidth(1000);
+        newsStage.setHeight(400);
+        */
+        detailStage.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
+            if (! isNowFocused) {
+                detailStage.hide();
+                //newsStage.hide();
+            }
+        });
 
-        subStage.show();
+        detailStage.show();
+        //newsStage.show();
     }
 }
