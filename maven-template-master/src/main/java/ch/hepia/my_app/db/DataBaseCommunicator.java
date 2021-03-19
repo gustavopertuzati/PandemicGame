3a1b9fd56052b4154a5880561c025b3ce29d3df5
Antwan <antoine.blancy@etu.hesge.ch>
2021-03-19 18:52:08 +0100

    Some work on new Database class

---
+++ maven-template-master/src/main/java/ch/hepia/my_app/db/DataBaseCommunicator.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+
+public class DataBaseCommunicator{
+  private String dbDriver;
+  private String url;
+  private String user;
+  private String pass;
+
+  public DataBaseCommunicator(String driver, String url, String user, String pass){
+    this.dbDriver = driver;
+    this.url = url;
+    this.user = user;
+    this.pass = pass;
+  }
+
+  public ResultSet executeQuery(String query) throws SQLException, ClassNotFoundException{
+    Class.forName(this.dbDriver);
+    String connection = this.url + "user=" + this.user  +"&password="+this.pass;
+    Connection co = DriverManager.getConnection(connection);
+    PreparedStatement pSt = co.prepareStatement(query);
+    co.close();
+    return pSt.executeQuery();
+  }
+  
+
+
+}
