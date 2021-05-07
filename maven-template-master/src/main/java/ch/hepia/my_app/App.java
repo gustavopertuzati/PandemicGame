cd49bb9ff194e97c50fa8b65bcba9fdbab74fc0f
Antwan <antoine.blancy@etu.hesge.ch>
2021-05-07 13:22:53 +0200

    lol

--- maven-template-master/src/main/java/ch/hepia/my_app/App.java
+++ maven-template-master/src/main/java/ch/hepia/my_app/App.java
@@ -126,7 +126,9 @@ public class App extends Application {
 
     public Circle getCountryCircle(Country c, ZoomableScrollPane pane) {
         Circle circ = new Circle(-1,-1,-1);
-        if(pane.getZoomWidth() < c.size()*800){
+
+        System.out.println(c.slug() + ":" + 1/(0.1*c.size()));
+        if(5000/pane.getZoomWidth() < 1/(0.1*c.size())){
             return circ;
         }
         circ = new Circle(c.coordinates()[0], c.coordinates()[1], 15 , c.getColorFromCountry());
